498a8f0d4c4bb73d9e8f860756fbf99cd3891989
Petrônio Cândido <petronio.candido@gmail.com>
2022-08-25 09:15:01 -0300

    Refatoração para o pacote apresentacao_console

--- src/main/java/br/edu/ifnmg/aplicacao_exemplo_spring/apresentacao_console/AplicacaoExemploSpringApplication.java
+++ src/main/java/br/edu/ifnmg/aplicacao_exemplo_spring/apresentacao_console/AplicacaoExemploSpringApplication.java
@@ -1,4 +1,4 @@
-package br.edu.ifnmg.aplicacao_exemplo_spring.aplicacao_exemplo_spring;
+package br.edu.ifnmg.aplicacao_exemplo_spring.apresentacao_console;
 
 //import javax.persistence.EntityManager;
 //import javax.persistence.EntityTransaction;
